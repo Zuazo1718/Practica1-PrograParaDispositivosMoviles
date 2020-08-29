5a62ec04505c056371205ddc0a6fd6942ac8fca8
Zuazo1718 <jezuma1718@gmail.com>
2020-08-29 11:06:38 -0400

    Hola mundo en android estudio Jeyson Zuazo

---
+++ app/src/main/java/com/zuazo/holamundozuazo/HolaMundoZuazo.java
@@ -0,0 +1,14 @@
+package com.zuazo.holamundozuazo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HolaMundoZuazo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hola_mundo_zuazo);
+    }
+}
